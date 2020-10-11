6563ccf26496f34c5d9fccbe545fc50a06587bff
Marco <46133335+Marco4413@users.noreply.github.com>
2020-10-11 16:59:33 +0200

    Updated example

--- example/script.js
+++ example/script.js
@@ -2,16 +2,25 @@
 import * as wcanvas from "../wcanvas.js";
 
 window.addEventListener("load", () => {
+    let time = 0;
+
     const wrappedCanvas = new wcanvas.wcanvas({
         "onSetup": (canvas) => {
             console.log("WCanvas Version: " + wcanvas.version);
             console.log("Canvas ID: " + canvas.canvas.id);
+
+            canvas.startLoop();
         },
-        "onDraw": (canvas) => {
+        "onDraw": (canvas, deltaTime) => {
+            time += deltaTime;
+            if (time >= 5) {
+                canvas.stopLoop();
+                console.log("Last Frame!");
+            }
+
+            console.log("Frame Time: " + deltaTime);
             canvas.context.fillStyle = "blue";
             canvas.context.fillRect(10, 10, 100, 100);
         }
     });
-
-    setInterval(wrappedCanvas.draw, 16);
 });
